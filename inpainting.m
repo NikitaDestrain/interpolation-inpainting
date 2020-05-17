a9351fa68982efa0b903ae0e25e19fa1dbaf5290
Nikita Govokhin <destra1n1337@gmail.com>
2020-05-18 03:48:18 +0400

    change

--- inpainting.m
+++ inpainting.m
@@ -1,20 +1,21 @@
 close all;
 clear; clc;
 
-dataset_path = 'cnn-results/DFNet_rectangle';
+dataset_path = 'classic_gauss';
 output_path = 'output';
 masks_path = 'masks';
-output_flist_name = 'flist';
-masks_flist_name = 'flist';
-result_path = 'classic_inpainting_2';
+output_flist_name = 'flist_hole';
+masks_flist_name = 'flist_mask';
+result_path = 'result';
 extension = '.jpg';
 
 image_height = 256;
 image_width = 256;
 dataset_size = 1228;
+sigma_max = 10;
 
 N = 92;
-sigma = floor(N/4);
+R = 15;
 
 disp('Candidate names reading...');
 candidates = read_flist([dataset_path, '/', output_path, '/', output_flist_name], dataset_size);
@@ -24,37 +25,71 @@ disp('Candidate names reading...');
 masks = read_flist([dataset_path, '/', masks_path, '/', masks_flist_name], dataset_size);
 disp('   success');
 
-for index=191:dataset_size
-    
-    candidate_full_path = [dataset_path, '/', output_path, '/', char(candidates(index))];
-    mask_full_path = [dataset_path, '/', masks_path, '/', char(masks(index))];
-    try
-        disp('Inpaint ' + candidates(index) + ' ...');
+for sigma=1:sigma_max
+    for index=1:dataset_size
         
-        candidate = imread(candidate_full_path);
-        result = candidate;
-        
-        %// Magic for the best border detection
-        mask = imread(mask_full_path);
-        gray_mask = rgb2gray(mask);
-        cont_mask = double(gray_mask);
-        cont_mask = cont_mask / max(cont_mask(:));
-        cont_mask(cont_mask < 0.8) = 0;
-        cont_mask(cont_mask >= 0.8) = 1;
-        
-        inpaintedR = uint8(inpaint(double(candidate(:,:,1)), double(cont_mask), N, sigma));
-        inpaintedG = uint8(inpaint(double(candidate(:,:,2)), double(cont_mask), N, sigma));
-        inpaintedB = uint8(inpaint(double(candidate(:,:,3)), double(cont_mask), N, sigma));
-        
-        res = cat(3, inpaintedR, inpaintedG, inpaintedB);
-        full_result_path = [result_path, '/', char(candidates(index))];
-        imwrite(res, full_result_path);
-    catch ME
-        disp([' ERROR! name: ', candidate_full_path, ' error: ', char(ME.identifier)]);
+        candidate_full_path = [dataset_path, '/', output_path, '/', char(candidates(index)), extension];
+        mask_full_path = [dataset_path, '/', masks_path, '/', char(masks(index)), extension];
+        try
+            disp('Inpaint ' + candidates(index) + ' ...');
+            
+            candidate = imread(candidate_full_path);
+            result = candidate;
+            
+            %// Magic for the best border detection
+            mask = imread(mask_full_path);
+
+            
+            gray_mask = rgb2gray(mask);
+            cont_mask = double(gray_mask);
+            cont_mask = cont_mask / max(cont_mask(:));
+            cont_mask(cont_mask < 0.8) = 0;
+            cont_mask(cont_mask >= 0.8) = 1;
+            
+            [startI, endI, startJ, endJ] = get_mask_coord(cont_mask);
+            
+            inpaintedR = inpaint(double(candidate(:,:,1)), double(cont_mask), N, sigma, R, startI, endI, startJ, endJ);
+            inpaintedG = inpaint(double(candidate(:,:,2)), double(cont_mask), N, sigma, R, startI, endI, startJ, endJ);
+            inpaintedB = inpaint(double(candidate(:,:,3)), double(cont_mask), N, sigma, R, startI, endI, startJ, endJ);
+           
+            res = cat(3, inpaintedR, inpaintedG, inpaintedB);
+            res = uint8(res);
+            
+            full_result_path = [result_path, '/', char(candidates(index)), extension];
+            imwrite(res, full_result_path);
+        catch ME
+            disp([' ERROR! name: ', candidate_full_path, ' error: ', char(ME.identifier)]);
+        end
     end
 end
 
-function [inpainted] = inpaint(aImage, aMask, aMaxHoleSize, aFilterSigma)
+function [startI, endI, startJ, endJ] = get_mask_coord(aMask)
+[imageW, imageH] = size(aMask);
+
+startI = -1;
+startJ = -1;
+
+for i=1:imageW
+    for j=1:imageH
+        if (aMask(i,j) == 0)
+            
+            if(startI == -1)
+                startI = i;
+            end
+            
+            if(startJ == -1)
+                startJ = j;
+            end
+            
+            endI = i;
+            endJ = j;
+            
+        end
+    end
+end
+end
+
+function inpainted = inpaint(aImage, aMask, aMaxHoleSize, aFilterSigma, R, startI, endI, startJ, endJ)
 [imageW, imageH] = size(aImage);
 mult = -0.5/ (aFilterSigma * aFilterSigma);
 inpainted = zeros(imageW,imageH);
@@ -66,33 +101,23 @@ for i=1:imageW
             continue;
         end
         
-        
         sum = 0.0;
         sumFilt = 0.0;
-        
         for v=-aMaxHoleSize:aMaxHoleSize
+            
+            indexI = i + v;
             for h=-aMaxHoleSize:aMaxHoleSize
-                indexI = i + v;
-                    if(indexI < 1)
-                       indexI = 1; 
-                    end
-                    if(indexI > 256)
-                       indexI = 256;
-                    end
-                    
-                    indexJ = j + h;
-                    if(indexJ < 1)
-                       indexJ = 1;
-                    end
-                    if(indexJ > 256)
-                       indexJ = 256;
-                    end
+                indexJ = j + h;
+                if ((indexI < 1) || (indexI > imageH) || (indexJ < 1)|| (indexJ > imageW))
+                    continue;
+                end
                 
-                if (aMask(indexI,indexJ) == 1)
+                if (aMask(indexI,indexJ) == 1 &&...
+                        indexI > (startI - R) && indexI < (endI + R) &&...
+                        indexJ > (startJ - R) && indexJ < (endJ + R))
+                   
                     valFilt = exp(mult * (v * v + h * h));
-                    
-                    
-                    
+ 
                     sumFilt = sumFilt + valFilt;
                     sum = sum + valFilt * aImage(indexI, indexJ);
                 end
@@ -103,6 +128,8 @@ for i=1:imageW
         inpainted(i,j) = sum / sumFilt;
     end
 end
+
+
 end
 
 function [names] = read_flist(path, size)
